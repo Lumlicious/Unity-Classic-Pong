421d14015411f8fe1fd26379001a3d6e04facf6d
Lumlicious <Lumlicious@users.noreply.github.com>
2018-07-22 17:43:01 -0500

    CHAD - First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public Ball ball;
+	public Paddle paddle;
+	public int xOffset = 20;
+	public int player1Score = 0;
+	public int player2Score = 0;
+
+	private Vector2 player1StartPosition;
+	private Vector2 player2StartPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+		// Get start positions for player 1 and 2 paddles
+		player1StartPosition = Camera.main.ScreenToWorldPoint(new Vector2(0 + xOffset, Screen.height / 2));
+		player2StartPosition = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width - xOffset, Screen.height / 2));
+
+		// Create ball
+		Instantiate(ball);
+		
+		// Create paddles for player 1 and 2
+		Paddle player1 = Instantiate(paddle, player1StartPosition, Quaternion.identity) as Paddle;
+		Paddle player2 = Instantiate(paddle, player2StartPosition, Quaternion.identity ) as Paddle;
+		player1.Init("player1");
+		player2.Init("player2");
+
+	}
+
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	[SerializeField]
+	float speed;
+
+	string input;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void Init(string player) {
+		input = player;
+		transform.name = player;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float velocity = Input.GetAxis(input) * Time.deltaTime * speed;
+		transform.Translate(velocity * Vector2.up);
+	}
+}
